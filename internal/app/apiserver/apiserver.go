1324ef7d62203701e4296a2081dc0ebd591f458d
akkitenshin <108996582+akkitenshin@users.noreply.github.com>
2023-07-20 12:08:08 +0300

    Update apiserver.go

--- internal/app/apiserver/apiserver.go
+++ internal/app/apiserver/apiserver.go
@@ -1,1 +1,37 @@
+package apiserver
 
+import (
+	"github.com/gorilla/mux"
+	"github.com/sirupsen/logrus"
+)
+
+type APIserver struct {
+	config *Config
+	logger *logrus.Logger
+	router *mux.Router
+}
+
+func New(config *Config) *APIserver {
+	return &APIserver{
+		config: config,
+		logger: logrus.New(),
+		router: mux.NewRouter(),
+	}
+}
+
+func (s *APIserver) Start() error {
+	if err := s.configureLogger(); err != nil{
+		return err
+	}
+
+	s.logger.Info("starting api server")
+
+	return nil
+}
+
+func (s *APIserver) configureLogger() error {
+	level, err := logrus.ParseLevel(s.config.Loglevel)
+	if err != nil{
+		return err
+	}
+}
